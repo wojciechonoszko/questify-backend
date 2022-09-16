1f16006a3e0f45044dc0dc78bffa90ad3bb0598b
michal-wolski <michal.wolski130@gmail.com>
2022-09-16 22:06:39 +0200

    add validation

--- controllers/task/addTask.js
+++ controllers/task/addTask.js
@@ -1,6 +1,11 @@
-const { Task, taskSchema } = require("../../models/taskSchema");
+const { Task, schemas } = require("../../models/taskSchema");
 
 const addTask = async (req, res) => {
+  const { error } = schemas.taskAddSchema.validate(req.body);
+  if (error) {
+    res.status(400).json({ message: "missing required name field" });
+    return;
+  }
   const result = await Task.create(req.body);
   res.status(201).json(result);
 };
--- controllers/task/getAllTask.js
+++ controllers/task/getAllTask.js
@@ -2,6 +2,10 @@ const { Task } = require("../../models/taskSchema");
 
 const getAllTask = async (req, res) => {
   const result = await Task.find({});
+  if (!result) {
+    res.status(404).json({ message: "Not found" });
+    return;
+  }
   res.json(result);
 };
 
